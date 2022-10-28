a56b2183eeed4fbd4bf31413c3248a5e02071732
Diego Paixão <diego.spaixao91@hotmail.com>
2022-10-28 14:22:54 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -4,7 +4,7 @@ let show2 = true;
 
 const menuSection = document.querySelector(".cabecalho")
 const menuToggle = menuSection.querySelector(".menu-toggle")
-const homeSection = document.querySelector(".home")
+const wrapperSection = document.querySelector(".wrapper")
 const logoSection = document.querySelector(".logo-responsive")
 
 menuToggle.addEventListener("click", () => {
@@ -12,7 +12,7 @@ menuToggle.addEventListener("click", () => {
     menuSection.classList.toggle("on", show)
     show = !show;
 
-    homeSection.classList.toggle("on", show1)
+    wrapperSection.classList.toggle("on", show1)
     show1 = !show2;
 
     logoSection.classList.toggle("off", show2)

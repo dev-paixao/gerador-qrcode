27277e844a108bdc7e528b57d4447c713a770686
Diego Paixão <diego.spaixao91@hotmail.com>
2023-05-23 21:47:53 -0300

    Refatorar geração do QR Code utilizando a API goqr.me
    Mudanças realizadas:
    - Atualizada a lógica de geração do QR Code para usar a API goqr.me.
    - Substituído o endpoint da API anterior pelo novo endpoint da API goqr.me.
    - Utilizada a função fetch() para fazer uma requisição AJAX à API goqr.me e obter a imagem do QR Code como um blob.
    - Atualizada a manipulação de eventos para garantir que a imagem do QR Code seja exibida corretamente e as alterações adequadas na interface sejam realizadas.

--- script.js
+++ script.js
@@ -2,18 +2,35 @@ const inputValue = document.querySelector('#inputValue');
 const btnValue = document.querySelector('#btnValue');
 const imgQrCode = document.querySelector('#imgQrCode');
 const wrapper = document.querySelector('.wrapper');
-let valueDefault;
+let valueDefault = '';
 
-// https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example 
-
-btnValue.addEventListener('click', () => {
-    let qrcodeValue = inputValue.value.trim();  
-    if(!qrcodeValue || qrcodeValue === valueDefault) return;
-    valueDefault = qrcodeValue;
-    btnValue.innerText = 'Gerando QR Code...'
-    imgQrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${valueDefault}`
-    imgQrCode.addEventListener('load', () => {
-        wrapper.classList.add('active');
-        btnValue.innerText = 'Gerar QRCode'
+// Função para gerar o QR Code
+function generateQRCode() {
+  const qrcodeValue = inputValue.value.trim();
+  
+  if (!qrcodeValue || qrcodeValue === valueDefault) {
+    return;
+  }
+  
+  valueDefault = qrcodeValue;
+  btnValue.innerText = 'Gerando QR Code...';
+  
+  const apiUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(valueDefault)}`;
+  
+  // Requisição AJAX para a API goqr.me
+  fetch(apiUrl)
+    .then(response => response.blob())
+    .then(blob => {
+      imgQrCode.src = URL.createObjectURL(blob);
+      wrapper.classList.add('active');
+      btnValue.innerText = 'Gerar QRCode';
     })
-})
+    .catch(error => {
+      console.error('Erro ao gerar o QR Code:', error);
+      btnValue.innerText = 'Gerar QRCode';
+    });
+}
+
+// Evento de clique no botão
+btnValue.addEventListener('click', generateQRCode);
+

82839735dfd6f85ff554853349427118bca398aa
Diego Paixão <diego.spaixao91@hotmail.com>
2023-05-24 22:35:08 -0300

    Atualização do layout e exibição do QR code em nova página
    Atualização do código do projeto e adição de uma nova página para exibir o QR code em uma aba separada. Além disso, fiz algumas melhorias na estrutura e no estilo da página.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,22 @@
+// script.js
 const inputValue = document.querySelector('#inputValue');
 const imgQrCode = document.querySelector('#imgQrCode');
-const qrcodeContainer = document.querySelector('.qrcode-container');
 const saveButton = document.querySelector('#saveButton');
 let valueDefault = '';
+let qrCodeGenerated = false; // Flag para verificar se um QR Code foi gerado
 
 // Função para gerar o QR Code
 function generateQRCode() {
   const qrcodeValue = inputValue.value.trim();
   
   if (!qrcodeValue || qrcodeValue === valueDefault) {
+    qrCodeGenerated = false;
     return;
   }
   
   valueDefault = qrcodeValue;
-  imgQrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(valueDefault)}`;
-  qrcodeContainer.classList.add('active');
+  imgQrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(valueDefault)}`;
+  qrCodeGenerated = true;
 }
 
 // Evento de digitação no campo de entrada
@@ -22,24 +24,12 @@ inputValue.addEventListener('keyup', generateQRCode);
 
 // Evento de clique no botão de salvar
 saveButton.addEventListener('click', function() {
-  // Cria um link temporário
-  const link = document.createElement('a');
-  
-  // Define o atributo href do link para a imagem do QR code
-  link.href = imgQrCode.src;
-  
-  // Define o atributo download para o nome do arquivo
-  link.download = 'qr-code.png';
-  
-  // Define o atributo target para "_blank" para abrir o link em uma nova aba
-  link.target = '_blank';
-  
-  // Adiciona o link temporário ao documento
-  document.body.appendChild(link);
-  
-  // Clica no link para iniciar o download
-  link.click();
-  
-  // Remove o link temporário do documento
-  document.body.removeChild(link);
+  // Verifica se um QR Code foi gerado
+  if (qrCodeGenerated) {
+    // Armazena a imagem do QR code no sessionStorage
+    sessionStorage.setItem('qrCodeSrc', imgQrCode.src);
+    
+    // Abre uma nova aba com a página qr-code.html
+    const newTab = window.open('qr-code.html', '_blank');
+  }
 });

946468b804bb8d535af49e554afb9e7fe421b2cc
Diego Paixão <diego.spaixao91@hotmail.com>
2023-05-24 21:33:27 -0300

    Atualizada a funcionalidade para salvar a imagem do QR Code gerado
    Atualização do código JavaScript para incluir a funcionalidade de salvar a imagem do QR Code gerado.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const inputValue = document.querySelector('#inputValue');
 const imgQrCode = document.querySelector('#imgQrCode');
-const wrapper = document.querySelector('.wrapper');
+const qrcodeContainer = document.querySelector('.qrcode-container');
+const saveButton = document.querySelector('#saveButton');
 let valueDefault = '';
 
 // Função para gerar o QR Code
@@ -13,8 +14,32 @@ function generateQRCode() {
   
   valueDefault = qrcodeValue;
   imgQrCode.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(valueDefault)}`;
-  wrapper.classList.add('active');
+  qrcodeContainer.classList.add('active');
 }
 
 // Evento de digitação no campo de entrada
 inputValue.addEventListener('keyup', generateQRCode);
+
+// Evento de clique no botão de salvar
+saveButton.addEventListener('click', function() {
+  // Cria um link temporário
+  const link = document.createElement('a');
+  
+  // Define o atributo href do link para a imagem do QR code
+  link.href = imgQrCode.src;
+  
+  // Define o atributo download para o nome do arquivo
+  link.download = 'qr-code.png';
+  
+  // Define o atributo target para "_blank" para abrir o link em uma nova aba
+  link.target = '_blank';
+  
+  // Adiciona o link temporário ao documento
+  document.body.appendChild(link);
+  
+  // Clica no link para iniciar o download
+  link.click();
+  
+  // Remove o link temporário do documento
+  document.body.removeChild(link);
+});
